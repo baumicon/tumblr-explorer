999c3976d3a242676c7d7bd50d5f6c2b2c41940c
Julien Kirch <code@archiloque.net>
2010-12-21 20:58:07 +0100

    starting display

--- public/tumblr-explorer.js
+++ public/tumblr-explorer.js
@@ -1,11 +1,27 @@
 var currentTumblr = null;
 
-var exploredTumblrs = null;
+var tumblrs = [];
 
 function displayMain() {
     var url = $("input[name=u]").val();
     if (url != "") {
         $.getJSON('/tumblr?u=' + url, function(data) {
+            currentTumblr = data;
+            tumblrs[data.url] = data;
+            $("#mainForm").slideUp(function() {
+                $("#explorerTitle").html(currentTumblr.name + " <a href='" + currentTumblr.url + "' target='_blank'>→</a>");
+                var content = "";
+                for(var i = 0; i < 25; i++) {
+                    var currentPost = currentTumblr.posts[i];
+                    currentPost.timestamp = new Date(currentPost.timestamp);
+                    content += "<div class='post'>" +
+                            "<div class='postImage'><img src='" + currentPost.small_image_url + "'></div>"+
+                            "<div class='postDate'>" + currentPost.timestamp.toLocaleString() + "</div>"+
+                            "</div>";
+                }
+                $("#explorerContent").html(content);
+                $("#explorer").slideDown();
+            })
         });
     }
 }
